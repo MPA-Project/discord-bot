f3cf44a6a2dbd151b51ceeda6f13dba270ab80dd
Alfian Rikzandi <denydirgantara94@gmail.com>
2021-11-01 20:16:06 +0700

    chore: add welcome image

--- src/cogs/welcome.py
+++ src/cogs/welcome.py
@@ -2,9 +2,8 @@ from discord import Forbidden, Embed, Member, File
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 from utils.welcome_image import image_generator
-import io
 import aiohttp
-import pyvips
+import os
 import aiofiles
 
 from ..db import db
@@ -34,29 +33,36 @@ class Welcome(Cog):
         # )
 
         async with aiohttp.ClientSession() as session:
-            welcome_url = image_generator(
-                name=member.display_name, avatar=member.avatar_url
+            # welcome_url = image_generator(
+            #     name=member.display_name, avatar=member.avatar_url
+            # )
+            welcome_url = "https://cardivo-dev.vercel.app/api/canvas?avatar={}&username={}&discriminator={}".format(
+                member.avatar_url, member.display_name, member.discriminator
             )
-            print(f"Welcome {welcome_url}")
+            # print(f"Welcome {welcome_url}")
             async with session.get(welcome_url) as resp:
                 if resp.status != 200:
                     await self.bot.get_channel(welcome_channel).send(
                         f"Welcome to **{member.guild.name}** {member.mention}!"
                     )
                 else:
-                    await self.bot.get_channel(welcome_channel).send(
-                        f"Welcome to **{member.guild.name}** {member.mention}!"
-                    )
+                    try:
+                        filename = f"./temp/welcome-{member.id}"
+                        file = await aiofiles.open(f"{filename}.png", mode="wb")
+                        await file.write(await resp.read())
+                        await file.close()
+                        await self.bot.get_channel(welcome_channel).send(
+                            f"Welcome to **{member.guild.name}** {member.mention}!",
+                            file=File(f"{filename}.png"),
+                        )
 
-                    filename = f"./temp/welcome-{member.id}"
-                    file = await aiofiles.open(f"{filename}.svg", mode="wb")
-                    await file.write(await resp.read())
-                    await file.close()
-                    temp_image = pyvips.Image.new_from_file(f"{filename}.svg", dpi=300)
-                    temp_image.write_to_file(f"{filename}.png")
-                    # await self.bot.get_channel(welcome_channel).send(
-                    #     file=File(f"{filename}.png")
-                    # )
+                        if os.path.isfile(f"{filename}.jpg"):
+                            os.remove(f"{filename}.jpg")
+
+                    except Exception:
+                        await self.bot.get_channel(welcome_channel).send(
+                            f"Welcome to **{member.guild.name}** {member.mention}!"
+                        )
 
         try:
             await member.send(f"Welcome to **{member.guild.name}**! Enjoy your stay!")
