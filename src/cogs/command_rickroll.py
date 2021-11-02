c8a69df02b7b269e10b05e889b8f0f03b35fa72f
Alfian Rikzandi <denydirgantara94@gmail.com>
2021-11-02 14:53:21 +0700

    chore: add rickroll

---
+++ src/cogs/command_rickroll.py
@@ -0,0 +1,40 @@
+from discord.ext.commands import Cog, command
+from discord import Message
+import aiohttp
+import aiofiles
+import os
+from discord import File
+
+
+class RickRoll(Cog):
+    def __init__(self, bot) -> None:
+        super().__init__()
+        self.bot = bot
+
+    @command(name="rickroll", hidden=True)
+    async def rickroll(self, ctx: Message):
+        url_pic = "https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url_pic) as resp:
+                if resp.status != 200:
+                    print(f"resp not 200 {resp}")
+                    pass
+                else:
+                    filename = f"./temp/rickroll-{ctx.author.id}"
+                    file = await aiofiles.open(f"{filename}.gif", mode="wb")
+                    await file.write(await resp.read())
+                    await file.close()
+
+                    await ctx.send(file=File(f"{filename}.gif"))
+
+                    if os.path.isfile(f"{filename}.gif"):
+                        os.remove(f"{filename}.gif")
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("command_rickroll")
+
+
+def setup(bot):
+    bot.add_cog(RickRoll(bot))
